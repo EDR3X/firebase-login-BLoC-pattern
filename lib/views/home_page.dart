91ead7ecd58d56474f28f881ed0c6569c3745f06
EDR3X <dhunganaannanta@gmail.com>
2022-07-17 13:06:01 +0545

    logout and profile page button added

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,7 @@
+import 'package:fireauth/blocs/auth/auth_bloc.dart';
+import 'package:fireauth/views/exports.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   static const String routeName = '/home-page';
@@ -10,7 +13,22 @@ class HomePage extends StatelessWidget {
       onWillPop: () async => false,
       child: Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: const Text("HomePage"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, ProfilePage.routeName);
+              },
+              icon: const Icon(Icons.account_circle),
+            ),
+            IconButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(SignoutEvent());
+              },
+              icon: const Icon(Icons.exit_to_app),
+            )
+          ],
         ),
         body: const Center(
           child: Text("Hello There"),
