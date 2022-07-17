ff88799c21b3e6adae9636681c6f5db6c0c2da9c
EDR3X <dhunganaannanta@gmail.com>
2022-07-17 11:39:30 +0545

    custom error dialogue utility function added

---
+++ lib/utils/error_dialogue.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import '../models/custom_error.dart';
+
+void errorDialogue(BuildContext context, CustomError e) {
+  print("code: ${e.code}\n message: ${e.message}\n plugin: ${e.plugin}\n");
+
+  if (Platform.isIOS) {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) => CupertinoAlertDialog(
+        title: Text(e.code),
+        content: Text('${e.plugin}\n ${e.message}'),
+        actions: [
+          CupertinoDialogAction(
+            child: const Text('Ok'),
+            onPressed: () => Navigator.pop(context),
+          )
+        ],
+      ),
+    );
+  } else if (Platform.isAndroid) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(e.code),
+        content: Text('${e.plugin}\n ${e.message}'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("OK"),
+          ),
+        ],
+      ),
+    );
+  }
+}
