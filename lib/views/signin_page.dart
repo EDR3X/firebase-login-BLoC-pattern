58894b5351027601786da48e4be2cbe64ba702ef
EDR3X <dhunganaannanta@gmail.com>
2022-07-17 11:40:35 +0545

    ux fixes and cubits added

--- lib/views/signin_page.dart
+++ lib/views/signin_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:validators/validators.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../utils/error_dialogue.dart';
 import '/blocs/exports.dart';
 import '/views/exports.dart';
 
@@ -37,109 +38,131 @@ class _SigninPageState extends State<SigninPage> {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () => FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        body: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30.0),
-            child: Form(
-              key: _formKey,
-              autovalidateMode: _autovalidateMode,
-              child: ListView(
-                shrinkWrap: true,
-                children: [
-                  Image.asset(
-                    'assets/images/flutter_logo.png',
-                    width: 250,
-                    height: 250,
-                  ),
-                  const SizedBox(
-                    height: 20.0,
-                  ),
-                  TextFormField(
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      labelText: 'Email',
-                      prefixIcon: Icon(Icons.email),
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return "Email is required";
-                      }
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: BlocConsumer<SigninCubit, SigninState>(
+          listener: (context, state) {
+            if (state.signInStatus == SignInStatus.error) {
+              errorDialogue(context, state.error);
+            }
+          },
+          builder: (context, state) {
+            return Scaffold(
+              body: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: _autovalidateMode,
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Image.asset(
+                          'assets/images/flutter_logo.png',
+                          width: 250,
+                          height: 250,
+                        ),
+                        const SizedBox(
+                          height: 20.0,
+                        ),
+                        TextFormField(
+                          autocorrect: false,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return "Email is required";
+                            }
 
-                      if (!isEmail(value.trim())) {
-                        return "Enter a valid email";
-                      }
-                      return null;
-                    },
-                    onSaved: (String? value) {
-                      _email = value;
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      filled: true,
-                      labelText: 'Password',
-                      prefixIcon: Icon(Icons.password),
-                    ),
-                    validator: (String? value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return "Password is required";
-                      }
+                            if (!isEmail(value.trim())) {
+                              return "Enter a valid email";
+                            }
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            _email = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            filled: true,
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.password),
+                          ),
+                          validator: (String? value) {
+                            if (value == null || value.trim().isEmpty) {
+                              return "Password is required";
+                            }
 
-                      if (value.trim().length < 6) {
-                        return "Password must be at least 6 characters long";
-                      }
+                            if (value.trim().length < 6) {
+                              return "Password must be at least 6 characters long";
+                            }
 
-                      return null;
-                    },
-                    onSaved: (String? value) {
-                      _password = value;
-                    },
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: _submit,
-                    style: ElevatedButton.styleFrom(
-                      textStyle: const TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 10),
+                            return null;
+                          },
+                          onSaved: (String? value) {
+                            _password = value;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          onPressed:
+                              state.signInStatus == SignInStatus.submitting
+                                  ? null
+                                  : _submit,
+                          style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            padding: const EdgeInsets.symmetric(vertical: 10),
+                          ),
+                          child: Text(
+                            state.signInStatus == SignInStatus.submitting
+                                ? 'Loading ...'
+                                : "Sign in",
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        TextButton(
+                          onPressed:
+                              state.signInStatus == SignInStatus.submitting
+                                  ? null
+                                  : () => Navigator.pushNamed(
+                                        context,
+                                        SignupPage.routeName,
+                                      ),
+                          style: TextButton.styleFrom(
+                            textStyle: const TextStyle(
+                              fontSize: 20,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                          child: const Text("Don't have account? Sign up!"),
+                        ),
+                      ],
                     ),
-                    child: const Text("Sign in"),
                   ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  TextButton(
-                    onPressed: () => Navigator.pushNamed(
-                      context,
-                      SignupPage.routeName,
-                    ),
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                        fontSize: 20,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                    child: const Text("Don't have account? Sign up!"),
-                  ),
-                ],
+                ),
               ),
-            ),
-          ),
+            );
+          },
         ),
       ),
     );
